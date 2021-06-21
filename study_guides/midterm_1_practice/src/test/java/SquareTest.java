47f770c252e61f1dc258e0744f4980057bb54d62
wonsunahn <wahn@pitt.edu>
2021-06-21 17:30:57 -0400

    Fixed some typos

--- study_guides/midterm_1_practice/src/test/java/SquareTest.java
+++ study_guides/midterm_1_practice/src/test/java/SquareTest.java
@@ -9,7 +9,7 @@ public class SquareTest {
 	/*
 	 * Preconditions: A new Square square is created
 	 *                A new Number number is created
-	 * Execution Steps: Call sq.setSquared(n, 3)
+	 * Execution Steps: Call square.setSquared(number, 3)
 	 * PostConditions: The value of number is set to 9
 	 */
 	@Test
